f5461a18e008f33420f23aba4b0396988630a502
michalkossakowski <141955539+michalkossakowski@users.noreply.github.com>
2024-11-23 00:23:24 +0100

    init routing

--- app.vue
+++ app.vue
@@ -1,53 +1,5 @@
 <template>
-  <div class="container mt-4">
-    <h1 class="text-center mb-4">Welcome back John Lennon</h1>
-
-    <main class="mt-4 d-flex justify-content-start align-items-start">
-
-      <nav class="nav flex-column me-4">
-        <RouterLink
-          to="/"
-          class="btn btn-success"
-          :class="{ active: $route.path === '/' }"
-        >
-          Home
-        </RouterLink>
-
-        <RouterLink
-          to="/servers"
-          class="btn btn-success"
-          :class="{ active: $route.path === '/servers' }"
-        >
-          Servers
-        </RouterLink>
-
-        <RouterLink
-          to="/channels"
-          class="btn btn-success"
-          :class="{ active: $route.path === '/channels' }"
-        >
-          Channels
-        </RouterLink>
-
-        <RouterLink
-          to="/messages"
-          class="btn btn-success"
-          :class="{ active: $route.path === '/messages' }"
-        >
-          Messages
-        </RouterLink>
-      </nav>
-
-
-      <div>
-
-        <RouterView />
-
-        <p>
-          <strong>Current route path:</strong> {{ $route.fullPath }}
-        </p>
-
-      </div>
-    </main>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
---
+++ layouts/default.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="full-width-container">
+        <h1 class="text-center">Welcome back John Lennon</h1>
+
+        <main class="mt-2 d-flex">
+        <nav class="nav flex-column me-4">
+            <NuxtLink
+            to="/"
+            class="btn btn-success mb-2"
+            :class="{ active: $route.path === '/' }"
+            >
+            Home
+            </NuxtLink>
+
+            <NuxtLink
+            to="/servers"
+            class="btn btn-success mb-2"
+            :class="{ active: $route.path === '/servers' }"
+            >
+            Servers
+            </NuxtLink>
+
+            <NuxtLink
+            to="/channels"
+            class="btn btn-success mb-2"
+            :class="{ active: $route.path === '/channels' }"
+            >
+            Channels
+            </NuxtLink>
+
+            <NuxtLink
+            to="/messages"
+            class="btn btn-success"
+            :class="{ active: $route.path === '/messages' }"
+            >
+            Messages
+            </NuxtLink>
+        </nav>
+
+        <div>
+            <NuxtPage />
+        </div>
+
+        <div class="custom-container d-flex justify-content-end">
+            <div class="to-right">
+                <h1>USER</h1>
+            </div>
+        </div>
+            
+
+        </main>
+    </div>
+</template>
+
+<style scoped>
+.to-right {
+    position: absolute;
+    right: 1vw;
+}
+</style>
