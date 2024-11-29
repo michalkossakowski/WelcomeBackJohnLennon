d8d2b8566108d9b17c56c1d9e3c3b7d8dd5d382f
Michał Kossakowski <kossakowskimichal2002@gmail.com>
2024-11-29 12:49:38 +0100

    nuxt api

--- pages/channels.vue
+++ pages/channels.vue
@@ -1,5 +1,10 @@
 <template>
     <div>
         <h1>Channles</h1>
+        <pre>{{ data }}</pre>
     </div>
 </template>
+
+<script setup lang="ts">
+    const { data } = await useFetch('/api/channels/get')
+</script>
